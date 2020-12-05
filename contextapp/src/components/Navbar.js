7d6a25013a685c8916f6674102f63bcbcb1700b4
Sarah Rosky <sarahrosky@gmail.com>
2020-12-05 13:37:23 -0800

    add working booklist context provider

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -4,17 +4,18 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends Component {
     static contextType = ThemeContext;
     render() {
-        console.log(this.context)
+        console.log(this.context);
         const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
-            </nav>
+            </nav >
         );
     }
 }
